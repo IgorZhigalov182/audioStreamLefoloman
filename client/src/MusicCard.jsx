18ed1447e02a358e5cfae7f2ebfa091fed6a70f5
Igor <zhigalovzenit112233@gmail.com>
2023-11-24 23:30:55 +0300

    Add Routing, MusicCard, RoomCard

--- client/src/MusicCard.jsx
+++ client/src/MusicCard.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import styles from './MusicCard.module.scss';
+import { useNavigate } from 'react-router-dom';
 
-const MusicCard = () => {
-  return <div>MusicCard</div>;
+const MusicCard = ({ name, id }) => {
+  const navigate = useNavigate();
+  const handleGoToRoom = () => navigate(`/rooms/${id}`, { state: 'test' });
+
+  return (
+    <div className={styles.card_wrapper} onClick={handleGoToRoom}>
+      <div className={styles.logo_card}></div>
+      <h1>{name}</h1>
+    </div>
+  );
 };
 
 export default MusicCard;
