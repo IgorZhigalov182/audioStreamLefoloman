7be525ed91bbb10534d423f65b5a278889b808e4
Geetork <galinaleespb@gmail.com>
2023-11-25 14:25:29 +0300

    room added

--- client/src/pages/room/Room.jsx
+++ client/src/pages/room/Room.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './Room.module.scss';
 
 const Room = () => {
-  return <div>Room</div>;
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
 };
 
 export default Room;
