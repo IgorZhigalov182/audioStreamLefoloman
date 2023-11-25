cc959da98c95a92bc986412a6f54b8cc074338bc
ibobrov <coderilya@gmail.com>
2023-11-26 07:33:56 +1000

    update scripts

--- server/db/scripts/009_dml_insert_rooms.sql
+++ server/db/scripts/009_dml_insert_rooms.sql
@@ -1,3 +1,6 @@
+INSERT INTO clients (login, password) VALUES
+('moderator1', 'pass'),
+('moderator2', 'pass');
 INSERT INTO rooms (name, description, moderator_id) VALUES
 ('First Room', 'very first room than have been created', 1),
 ('Best Room', 'this is room you need!!!', 2);
