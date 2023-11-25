e72c884f15c38ffca12a2742b374a1cd0e4cf05c
ibobrov <coderilya@gmail.com>
2023-11-26 09:40:28 +1000

    add server-chat

---
+++ chat/src/main/java/com/example/chat/ChatRunner.java
@@ -0,0 +1,12 @@
+package com.example.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRunner.class, args);
+	}
+}
